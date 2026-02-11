ba2df732ec68cf8f711b60a2c5b4a5584f6ef2f2
Haran1772004 <hariharank.ug22.cs@francisxavier.ac.in>
2026-02-11 06:52:26 +0000

    to check haridummy

--- googleForms/urls.py
+++ googleForms/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views # Correctly imported
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Root path for testing CI/CD
-    path('', views.home, name='home'),
+    
 
     # Existing API paths
     path('api/accounts/', include('accounts.urls')),
--- googleForms/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>CI/CD Automation is Live!</h1><p>If you see this, your GitHub Action worked.</p>")
