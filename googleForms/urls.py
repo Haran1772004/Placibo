bbd269f4d4a73d4ebce30b438be3ecf11eed3584
Haran1772004 <hariharank.ug22.cs@francisxavier.ac.in>
2026-02-11 10:01:29 +0000

    eg1

--- googleForms/urls.py
+++ googleForms/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views  # Added this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Root path for testing CI/CD
-    
+    # Root path for testing CI/CD - Added this line
+    path('', views.home, name='home'), 
 
     # Existing API paths
     path('api/accounts/', include('accounts.urls')),
---
+++ googleForms/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>CI/CD Pipeline & Django Site are Live!</h1>")
