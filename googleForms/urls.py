4a3d396dae8c431664916c1ca31fce7a0ff0c517
Haran1772004 <hariharank.ug22.cs@francisxavier.ac.in>
2026-02-11 06:23:17 +0000

    fix

--- googleForms/urls.py
+++ googleForms/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     # The Django Admin interface
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 
     # This connects your accounts/urls.py logic
     # Access this via: http://127.0.0.1:8000/api/accounts/users/
